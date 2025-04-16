28e8510ef467498fade47cb0c8cbfbfd3f294265
young sik seo <angelskying@gmail.com>
2025-04-16 13:33:06 +0900

    Comment out unused imports in Home component for clarity and maintainability.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { useGameStore } from '@/store/gameStore';
 import { useRouter } from 'next/navigation';
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 
 export default function Home() {
 	const { initGame } = useGameStore();
